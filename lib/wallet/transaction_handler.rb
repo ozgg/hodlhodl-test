4c8acb27f2e470b10b5bb0e66f4c113333adac15
Maxim Khan-Magomedov <maxim.km@gmail.com>
2025-06-23 02:39:10 +0300

    Improve code structure

--- lib/wallet/transaction_handler.rb
+++ lib/wallet/transaction_handler.rb
@@ -8,9 +8,9 @@ module Wallet
   # Transaction handler
   # rubocop:disable Naming/MethodParameterName
   class TransactionHandler
-    FEE_API_URL = 'https://mempool.space/api/v1/fees/recommended'
-    DEFAULT_FEE_RATE = 4.0
-    MIN_FEE_RATE = 2.0 # Minimal safe rate to avoid "min relay fee not met" error
+    FEE_API_URL = 'https://mempool.space/signet/api/v1/fees/recommended'
+    DEFAULT_FEE_RATE = 10.0
+    MIN_FEE_RATE = 3.0 # Safe rate to avoid "min relay fee not met" error
 
     # @param [Bitcoin::Key] key
     def initialize(key)
@@ -29,44 +29,19 @@ module Wallet
     #
     # @see https://github.com/chaintope/bitcoinrb/wiki/Transaction
     #
+    # Calculates recommended fee, signs and sends transaction
+    #
     # @param [String] to_address
     # @param [Float] amount_btc
     def transfer(to_address, amount_btc)
       amount_sats = (amount_btc * 100_000_000).to_i
-      balance = @mempool.balance(@utxos)
+      unsigned_tx = build_unsigned_tx(to_address, amount_sats)
+      change = @mempool.balance(@utxos) - amount_sats - estimate_tx_fee(unsigned_tx)
 
-      tx = build_unsigned_tx(to_address, amount_sats)
-      estimate_fee = estimate_tx_fee(tx)
+      raise 'Insufficient funds' if change.negative?
 
-      raise 'Insufficient funds' if balance < amount_sats + estimate_fee
-
-      change = balance - amount_sats - estimate_fee
-
-      tx = Bitcoin::Tx.new
-      tx.version = 2
-
-      # Use all confirmed UTXOs
-      @utxos.each do |utxo|
-        tx.in << Bitcoin::TxIn.new(out_point: Bitcoin::OutPoint.new(utxo['txid'].rhex, utxo['vout']))
-      end
-
-      tx.out << output(amount_sats, to_address)
-      tx.out << output(change, @key.to_p2wpkh) if change.positive?
-
-      script_pubkey = Bitcoin::Script.parse_from_addr(@key.to_p2wpkh)
-
-      # Sign transaction
-      @utxos.each_with_index do |utxo, i|
-        amount = utxo['value']
-        sighash = tx.sighash_for_input(i, script_pubkey, amount:, sig_version: :witness_v0)
-        sig = @key.sign(sighash) + [Bitcoin::SIGHASH_TYPE[:all]].pack('C')
-
-        tx.in[i].script_witness.stack << sig
-        tx.in[i].script_witness.stack << @key.pubkey.htb
-      end
-
-      raw_tx = tx.to_hex
-      @mempool.broadcast(raw_tx)
+      signed_tx = build_and_sign_tx(to_address, amount_sats, change)
+      @mempool.broadcast(signed_tx.to_hex)
     end
 
     private
@@ -80,13 +55,43 @@ module Wallet
       tx = Bitcoin::Tx.new
       tx.version = 2
 
-      @utxos.each do |utxo|
-        puts "Using input: #{utxo['txid']}:#{utxo['vout']}"
-        tx.in << Bitcoin::TxIn.new(out_point: Bitcoin::OutPoint.new(utxo['txid'], utxo['vout']))
-      end
+      @utxos.each { |utxo| tx.in << input(utxo) }
 
       tx.out << output(amount, to_address)
       tx
+    end
+
+    # Build signed transaction
+    #
+    # @param [String] to_address
+    # @param [Integer] amount_sats
+    # @param [Integer] change
+    # @return [Bitcoin::Tx]
+    def build_and_sign_tx(to_address, amount_sats, change)
+      tx = Bitcoin::Tx.new
+      tx.version = 2
+
+      @utxos.each { |utxo| tx.in << input(utxo) }
+
+      tx.out << output(amount_sats, to_address)
+      tx.out << output(change, @key.to_p2wpkh) if change.positive?
+
+      sign_tx(tx)
+      tx
+    end
+
+    # Sign transaction
+    #
+    # @param [Bitcoin::Tx] tx
+    def sign_tx(tx)
+      script_pubkey = Bitcoin::Script.parse_from_addr(@key.to_p2wpkh)
+
+      @utxos.each_with_index do |utxo, i|
+        sighash = tx.sighash_for_input(i, script_pubkey, amount: utxo['value'], sig_version: :witness_v0)
+        sig = @key.sign(sighash) + [Bitcoin::SIGHASH_TYPE[:all]].pack('C')
+
+        tx.in[i].script_witness = Bitcoin::ScriptWitness.new([sig, @key.pubkey.htb])
+      end
     end
 
     # Generate transaction output
@@ -97,14 +102,19 @@ module Wallet
       Bitcoin::TxOut.new(value:, script_pubkey: Bitcoin::Script.parse_from_addr(address))
     end
 
+    # @param [Hash] utxo
+    def input(utxo)
+      Bitcoin::TxIn.new(out_point: Bitcoin::OutPoint.new(utxo['txid'].rhex, utxo['vout']))
+    end
+
     # @param [Bitcoin::Tx] tx
     def estimate_tx_fee(tx)
       dummy_key = @key.pubkey.htb
-      dummy_sig = Array.new(72, 0) # Usual signature size is 70 to 72 bytes
+      dummy_sig = Array.new(72, 0).pack('C*') # Usual signature size is 70 to 72 bytes
 
       # Adding dummy witnesses
       tx.in.each_with_index do |_, i|
-        tx.in[i].script_witness = Bitcoin::ScriptWitness.new([dummy_sig.pack('C*'), dummy_key])
+        tx.in[i].script_witness = Bitcoin::ScriptWitness.new([dummy_sig, dummy_key])
       end
 
       # 4 vbytes per weight unit
@@ -112,7 +122,7 @@ module Wallet
       (vbytes * fee_rate).ceil
     end
 
-    # Receive recommended fee rate for 'hourFee'
+    # Receive recommended fee rate for 'fastestFee'
     #
     # @see https://mempool.space/graphs/mining/block-fee-rates
     #
@@ -120,9 +130,9 @@ module Wallet
     #
     # return [Float]
     def recommended_fee_rate
-      response = HTTParty.get('https://mempool.space/api/v1/fees/recommended', timeout: 3)
+      response = HTTParty.get(FEE_API_URL, timeout: 3)
       if response.success?
-        [(response['hourFee'] || DEFAULT_FEE_RATE).to_f, MIN_FEE_RATE].max
+        [(response['fastestFee'] || DEFAULT_FEE_RATE).to_f, MIN_FEE_RATE].max
       else
         warn "Failed to fetch recommended fee rate: HTTP #{response.code}, using default"
         DEFAULT_FEE_RATE
